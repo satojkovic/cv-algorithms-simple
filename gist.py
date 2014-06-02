7798924c6b6fcf71914bfa70da37c230ad1bf906
satojkovic <satojkovic@gmail.com>
2014-06-03 00:45:13 +0900

    Main process

--- gist.py
+++ gist.py
@@ -4,22 +4,10 @@ import argparse
 import cv2
 import sys
 
-NBLOCKS = 4
-NSCALE = 3
 
 def color_gist_scaletab(img, w, n_scale, n_orientation):
-    src_width, src_height = img.shape[0], img.shape[1]
-
-    if src_width < 8 or src_height < 8:
-        print 'Error: color_gist_scaletab() - Image not big enough'
-        sys.exit(-1)
-
-    numberBlocks = w
-    tot_oris = 0
-    for i in xrange(n_scale):
-        tot_oris += n_orientation[i]
-
-    copy_img = img.copy()
+    desc = [0 for i in xrange(960)]
+    return desc
     
 def main():
     parser = argparse.ArgumentParser(
@@ -34,9 +22,30 @@ def main():
     args = parser.parse_args()
 
     img = cv2.imread(args.imgfile)
-    if img:
+    if img == None:
         print 'Could not load image: %s' % args.imgfile
         sys.exit(-1)
+
+    nblocks = 4
+    n_scale = 3
+    orientations_per_scale = [0 for i in xrange(50)]
+    orientations_per_scale[0] = 8
+    orientations_per_scale[0] = 8
+    orientations_per_scale[0] = 4
+
+    desc = color_gist_scaletab(img,
+                               nblocks,
+                               n_scale,
+                               orientations_per_scale)
+
+    descsize = 0
+    for i in xrange(n_scale):
+        descsize += nblocks*nblocks*orientations_per_scale[i]
+    descsize *= 3
+
+    for i in xrange(descsize):
+        print "%.4f" % desc[i]
+    print "\n"
 
 if __name__ == '__main__':
     main()
