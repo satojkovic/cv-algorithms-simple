e705c983dbfe12cd509ae2180d2696c791585186
satojkovic <satojkovic@gmail.com>
2014-11-01 21:30:26 +0900

    Add CalcHist and ShowHist func

--- cvHist/histmain.go
+++ cvHist/histmain.go
@@ -8,13 +8,27 @@ package main
 import "C"
 import "unsafe"
 
-func main() {
-	filename := C.CString("snow_leopard.jpg")
+type Hist struct {
+	img unsafe.Pointer
+}
+
+func CalcHist(fn string) (*Hist, error) {
+	filename := C.CString(fn)
 	defer C.free(unsafe.Pointer(filename))
 
-	C.cvNamedWindow(filename, 1)
-
 	img := unsafe.Pointer(C.cvLoadImage(filename, C.CV_LOAD_IMAGE_COLOR))
-	C.cvShowImage(filename, img)
+	return &Hist{img}, nil
+}
+
+func (h *Hist) ShowHist(wn string) {
+	windowname := C.CString(wn)
+	defer C.free(unsafe.Pointer(windowname))
+	C.cvShowImage(windowname, h.img)
 	C.cvWaitKey(0)
+}
+
+func main() {
+	fn := "snow_leopard.jpg"
+	h, _ := CalcHist(fn)
+	h.ShowHist("Histogram")
 }
