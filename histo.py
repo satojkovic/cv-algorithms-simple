5f8e0df6c479457c2a2b861ba14222f9b9a76e7f
satojkovic <satojkovic@gmail.com>
2015-11-21 11:18:00 +0900

    Parse args

--- histo.py
+++ histo.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def main():
-    pass
+"""
+Usage:
+    histo.py <image_file>
+    histo.py -h | --help
+Options:
+    -h --help    show help message
+"""
+import cv2
+from docopt import docopt
+import os
+import sys
 
+
+def main():
+    args = docopt(__doc__)
+    image_file = args['<image_file>']
+    if not os.path.exists(image_file):
+        print 'Not found:', image_file
+        sys.exit(-1)
 
 if __name__ == '__main__':
     main()
