3d4e6180bfa525f7ffd027f28b63e1772ff97a9f
satojkovic <satojkovic@gmail.com>
2016-02-07 21:49:14 +0900

    Use cv2.circle

--- mser_sample.py
+++ mser_sample.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import numpy as np
 import cv2
 
+
 def main():
-    img = cv2.imread('toy_in.png')
+    img = cv2.imread('checker_board.jpg')
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ms = cv2.MSER()
     msers = ms.detect(gray_img)
@@ -13,9 +13,13 @@ def main():
         color = (np.random.randint(0, 256),
                  np.random.randint(0, 256),
                  np.random.randint(0, 256))
-        cv2.drawContours(img, msers, i, color)
+        for j in range(len(msers[i])):
+            pos = (msers[i][j][0], msers[i][j][1])
+            cv2.circle(img, pos, 1, color)
     cv2.imshow('TEST', img)
     cv2.waitKey(0)
+
+    cv2.imwrite('checker_board_gray_result.jpg', img)
 
 
 if __name__ == '__main__':
